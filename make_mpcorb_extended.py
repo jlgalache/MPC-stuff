4ca4790c72c97c25add9f4c477bcbbef90f486f6
J.L. Galache <jlgalache@gmail.com>
2016-03-16 13:21:15 -0400

    Commiting

--- make_mpcorb_extended.py
+++ make_mpcorb_extended.py
@@ -11,19 +11,25 @@ import time
 from time import strftime
 import urllib.request
 import json
+import shutil
+from shutil import copy2
+import gzip
 
 
+# Copy MPCORB.DAT when running locally in /base/public/:
+#copy2('iau/MPCORB/MPCORB.DAT', 'mpcorb.dat')
 
-#try:
-  #urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT', 'mpcorb.dat')
-#except Exception as the_error:
-  #localtime = time.localtime(time.time())
-  #with open('make_mpcorb_extended.log', 'a') as log_file:
-    #log_file.write(strftime('%Y/%m/%d at %H:%M:%S\n  Problem downloading file: \n  http://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT\n  Error type: '+str(the_error)+'\n', localtime))
-  #sys.exit(1)
+# Get MPCORB.DAT over the internet:
+try:
+  urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT', 'mpcorb.dat')
+except Exception as the_error:
+  localtime = time.localtime(time.time())
+  with open('make_mpcorb_extended.log', 'a') as log_file:
+    log_file.write(strftime('%Y/%m/%d at %H:%M:%S\n  Problem downloading file: \n  http://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT\n  Error type: '+str(the_error)+'\n', localtime))
+  sys.exit(1)
 
 # Find length of header:
-with open('mpcorb_test.dat', 'r') as data_file:
+with open('mpcorb.dat', 'r') as data_file:
   c = 0
   for  line in data_file:
     c = c + 1
@@ -70,7 +76,7 @@ for item in dbl_in:
 ids = ids + dbl
 
 try:
-  data_file = open('mpcorb_test.dat', 'rb')
+  data_file = open('mpcorb.dat', 'rb')
 except IOError:
   localtime = time.localtime(time.time())
   with open('make_mpcorb_extended.log', 'a') as log_file:
@@ -83,7 +89,7 @@ sys.stderr.write(strftime('%Y/%m/%d at %H:%M:%S\n  ', localtime))
 
 
 output_file = open('mpcorb_extended.dat', 'w')         
-with open('mpcorb_test.dat', 'r') as data_file:
+with open('mpcorb.dat', 'r') as data_file:
   head = list(islice(data_file, num_header_lines))
   for item in head:
     output_file.write(item)
@@ -96,7 +102,7 @@ with open('mpcorb_test.dat', 'r') as data_file:
       epoch = me.convert_packed_date_to_jd(epoch)
       M = float(line[26:35])
       n = float(line[81:90])
-# Calculate Time of Periastron
+# Calculate Time of Periastron:
       if M >= 180.0:
         Tp = epoch + (360.0-M)/n
       else:
@@ -147,6 +153,10 @@ output_file.close()
 output_json_file = open('mpcorb_extended.json', 'w')
 output_json = []
 
+
+neo_flag_file = open('neo_flag_file', 'w')
+
+
 with open('mpcorb_extended.dat', 'r') as data_file:
   head = list(islice(data_file, num_header_lines))
   for line in data_file:
@@ -173,7 +183,95 @@ with open('mpcorb_extended.dat', 'r') as data_file:
       Q_big = a*(1.0 + e)
       semilatus = (a/2.0)*(1.0 - e**2.0)
       period = math.sqrt(a**3.0)
+      synodic = 1.0/(abs((1.0)-(1.0/period)))
       last_obs = line[194:198]+'-'+line[198:200]+'-'+line[200:202]
+      hex_flags = line[161:165].strip()
+      if hex_flags[3] == '0':
+        orbit_type = 'MBA'
+      elif hex_flags[3] == '1':
+        orbit_type = 'Atira'
+      elif hex_flags[3] == '2':
+        orbit_type = 'Aten'
+      elif hex_flags[3] == '3':
+        orbit_type = 'Apollo'
+      elif hex_flags[3] == '4':
+        orbit_type = 'Amor'
+      elif hex_flags[3] == '5':
+        orbit_type = 'Object with perihelion distance < 1.665 AU'
+      elif hex_flags[3] == '6':
+        orbit_type = 'Hungaria'
+      elif hex_flags[3] == '7':
+        orbit_type = 'Phocaea'
+      elif hex_flags[3] == '8':
+        orbit_type = 'Hilda'
+      elif hex_flags[3] == '9':
+        orbit_type = 'Jupiter Trojan'
+      elif hex_flags[3] == 'A':
+        orbit_type = 'Distant Object'
+      else:
+        sys.stderr.write('  Unknown orbit type, '+hex_flags[3]+' for object: '+desig+'\n')
+        sys.stderr.close()
+        break
+        
+      if hex_flags[1] == '8':
+        neo_flag = 1
+      elif hex_flags[1] == '0':
+        neo_flag = 0
+      else:
+        neo_flag_file.write(desig+' -- '+hex_flags[1]+'\n')
+        
+      km_neo_flag = 0
+      opp_flag = 0
+      critical_flag = 0
+      pha_flag = 0
+      if int(hex_flags[0],16) == 1:
+        km_neo_flag = 1
+      elif int(hex_flags[0],16) == 2:
+        opp_flag = 1
+      elif int(hex_flags[0],16) == 3:
+        km_neo_flag = 1
+        opp_flag = 1
+      elif int(hex_flags[0],16) == 4:
+        critical_flag = 1
+      elif int(hex_flags[0],16) == 5:
+        km_neo_flag = 1
+        critical_flag = 1
+      elif int(hex_flags[0],16) == 6:
+        opp_flag = 1
+        critical_flag = 1
+      elif int(hex_flags[0],16) == 7:
+        km_neo_flag = 1
+        opp_flag = 1
+        critical_flag = 1
+      elif int(hex_flags[0],16) == 8:
+        pha_flag = 1
+      elif int(hex_flags[0],16) == 9:
+        km_neo_flag = 1
+        pha_flag = 1
+      elif int(hex_flags[0],16) == 10:
+        opp_flag = 1
+        pha_flag = 1
+      elif int(hex_flags[0],16) == 11:
+        km_neo_flag = 1
+        opp_flag = 1
+        pha_flag = 1
+      elif int(hex_flags[0],16) == 12:
+        critical_flag = 1
+        pha_flag = 1
+      elif int(hex_flags[0],16) == 13:
+        km_neo_flag = 1
+        critical_flag = 1
+        pha_flag = 1
+      elif int(hex_flags[0],16) == 14:
+        opp_flag = 1
+        critical_flag = 1
+        pha_flag = 1
+      elif int(hex_flags[0],16) == 15:
+        km_neo_flag = 1
+        opp_flag = 1
+        critical_flag = 1
+        pha_flag = 1
+      
       H = line[8:13].strip()
       if H != '':
         H = float(H)
@@ -196,7 +294,7 @@ with open('mpcorb_extended.dat', 'r') as data_file:
       else:
         arc_length = ''
         arc_years = arc
-      output_json.append({'number':number,'Name':name,'Designation':desig,'Other_desigs':other_desig,'H':H,'G':G,'Epoch':round(epoch,7),'M':float(line[26:35].strip()),'Peri':float(line[37:46].strip()),'Node':float(line[48:57].strip()),'i':float(line[59:68].strip()),'e':float(line[70:79].strip()),'n':float(line[80:91].strip()),'a':float(line[92:103].strip()),'U':U,'ref':line[107:116].strip(),'Num_obs':num_obs,'Num_opps':int(line[123:126].strip()),'Arc_length':arc_length,'Arc_years':arc_years,'rms':rms,'Perturbers':line[142:145].strip(),'Perturbers_2':line[146:149].strip(),'Computer':line[150:160].strip(),'Hex_flags':line[161:165].strip(),'Last_obs':last_obs,'Tp':float(line[203:216].strip()),'Orbital_period':round(period,7),'Perihelion_dist':round(q_small,7),'Aphelion_dist':round(Q_big,7),'Semilatus_rectum':round(semilatus,7)})
+      output_json.append({'number':number,'Name':name,'Designation':desig,'Other_desigs':other_desig,'H':H,'G':G,'Epoch':round(epoch,7),'M':float(line[26:35].strip()),'Peri':float(line[37:46].strip()),'Node':float(line[48:57].strip()),'i':float(line[59:68].strip()),'e':float(line[70:79].strip()),'n':float(line[80:91].strip()),'a':float(line[92:103].strip()),'U':U,'ref':line[107:116].strip(),'Num_obs':num_obs,'Num_opps':int(line[123:126].strip()),'Arc_length':arc_length,'Arc_years':arc_years,'rms':rms,'Perturbers':line[142:145].strip(),'Perturbers_2':line[146:149].strip(),'Computer':line[150:160].strip(),'Hex_flags':hex_flags,'Last_obs':last_obs,'Tp':float(line[203:216].strip()),'Orbital_period':round(period,7),'Perihelion_dist':round(q_small,7),'Aphelion_dist':round(Q_big,7),'Semilatus_rectum':round(semilatus,7),'Synodic_period':round(synodic,7),'NEO_flag':neo_flag,'1-km_neo_flag':km_neo_flag,'1-opposition_object_flag':opp_flag,'Critical_list_numbered_object_flag':critical_flag,'PHA_flag':pha_flag})
       #output_json_file.write('{"number":"'+number+'",\n')
       #output_json_file.write('"Name":"'+name+'",\n')
       #output_json_file.write('"Desig":"'+desig+'",\n')
@@ -237,7 +335,17 @@ with open('mpcorb_extended.dat', 'r') as data_file:
 json.dump(output_json, output_json_file, indent=0)
 output_json_file.close()
 
+with open('mpcorb_extended.dat', 'rb') as input_file:
+  with gzip.open('mpcorb_extended.dat.gz', 'wb') as output_file:
+    shutil.copyfileobj(input_file, output_file)
+
+with open('mpcorb_extended.json', 'rb') as input_file:
+  with gzip.open('mpcorb_extended.json.gz', 'wb') as output_file:
+    shutil.copyfileobj(input_file, output_file)
+
 sys.stderr.write('Program finished without known errors\n')
 sys.stderr.close()
 sys.stderr = sys.__stderr__
 
+
+neo_flag_file.close()
