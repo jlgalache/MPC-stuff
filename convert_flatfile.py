487805aff72ded23cb61358a9f458447a1e84551
J.L. Galache <jlgalache@gmail.com>
2016-03-23 13:42:30 -0400

    Some new stuff

--- convert_flatfile.py
+++ convert_flatfile.py
@@ -2,26 +2,83 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import json
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-in','--flat_filename', help='Name of the file you want to convert')
-parser.add_argument('-out','--output_filename', help='Name of the output file')
-parser.add_argument('-s','--separator', help='Column separator')
-parser.add_argument('-cs','--col_start', help='Column start')
-parser.add_argument('-cw','--col_width', help='Column width')
-parser.add_argument('-ct','--col_type', help='Column data type')
-parser.add_argument('-cn','--col_names', help='Column names if you don\'t want to use the header or it\'s missing')
+#parser.add_argument('-in','--flat_filename', help='Name of the file you want to convert')
+#parser.add_argument('-out','--output_filename', help='Name of the output file')
+parser.add_argument('flat_filename', help='Name of the file you want to convert')
+parser.add_argument('output_filename', help='Name of the output file')
+parser.add_argument('-hl','--header_lines', default=-1, type=int, help='Number of header lines')
+parser.add_argument('-cnl','--col_name_line', default=-1, type=int, help='Line number where column names are found')
+parser.add_argument('-nl','--num_rows', default=-1, type=int, help='Number of data rows to read')
+parser.add_argument('-s','--separator', default='', help='Column separator')
+parser.add_argument('-cs','--col_start', nargs='*', type=int, help='Column start')
+parser.add_argument('-cw','--col_width', nargs='*', type=int, help='Column width')
+parser.add_argument('-ct','--col_type', nargs='*', help='Column data type')
+parser.add_argument('-cn','--col_names', nargs='*', default='', type=str, help='Column names if you don\'t want to use the header or it\'s missing')
 args = parser.parse_args()
 
-print(' Input file: '+args.flat_filename)
-print(' Output file: '+args.output_filename)
+print(' Input file: '+args.flat_filename+' and length is: '+str(len(args.flat_filename)))
+print(' Output file: '+args.output_filename+' and length is: '+str(len(args.output_filename)))
+print(' Column starts: '+str(args.col_start)+' and length is: '+str(len(args.col_start)))
+print(' Column width: '+str(args.col_width)+' and length is: '+str(len(args.col_width)))
+print(' Column type: '+str(args.col_type)+' and length is: '+str(len(args.col_type)))
+print(' Column names: '+str(args.col_names)+' and length is: '+str(len(args.col_names)))
+print(' CNL: '+str(args.col_name_line))
 
 input_file = open(args.flat_filename, 'r')
 
+# Get column names from the appropriate line or from the provided names
+if args.col_name_line != -1:
+  for read_count, line in enumerate(input_file):
+    print('  count is: '+read_count)
+    if (read_count+1) != args.col_name_line:
+      duff = input_file.readline()
+    else:
+      headings = []
+      for i, j in enumerate(args.col_start):
+        headings.append(str.strip(line[j:j+args.col_width[i]]))
+      break
+else:
+  if args.col_names == '':
+    print('\n You\'re missing -cnl *and* -cn. You need one or the other to define the column names.\n')
+    sys.exit(1)
+  headings = args.col_names
+  read_count = 0
+
+# Get to the line where the data starts
+if args.header_lines > read_count:
+  for i, line in enumerate(input_file): 
+    if i == (args.header_lines - read_count - 1):
+      break
+
+output_json_file = open(args.output_filename, 'w')
+output_json = []
+
+# Start reading in data
 for line in input_file:
-  headings = []
-  for count, i in enumerate(args.col_start):
-    headings.append(line[i:i+args.col_width[count]])
-    
-print(headings)
+  print(line.rstrip())
+  line_dict = {}
+  for  i, j in enumerate(args.col_start):
+    print(' i = '+str(i)+', j = '+str(j)+' -- args.col_width = '+str(args.col_width[i])+' -- heading: '+str(headings[i]))
+    if str.strip(line[j:j+args.col_width[i]]) != '':
+      if args.col_type[i] == 'int':
+        line_dict[headings[i]] = int(str.strip(line[j:j+args.col_width[i]]))
+        #line_dict.append({str(headings[i]):int(str.strip(line[j:j+args.col_width[i]]))})
+      elif args.col_type[i] == 'flt':
+        line_dict[headings[i]] = float(str.strip(line[j:j+args.col_width[i]]))
+        #line_dict.append({str(headings[i]):float(str.strip(line[j:j+args.col_width[i]]))})
+      elif args.col_type[i] == 'str':
+        line_dict[headings[i]] = str.strip(line[j:j+args.col_width[i]])
+        #line_dict.append({str(headings[i]):str.strip(line[j:j+args.col_width[i]])})
+  output_json.append(line_dict)
+
+#print(headings)
+#print(str(count))
+
+
+json.dump(output_json, output_json_file, indent=0)
+output_json_file.close()
+input_file.close()
--- make_mpcorb_extended_backup_2016-03-23.py
+++ make_mpcorb_extended_backup_2016-03-23.py
@@ -15,19 +15,15 @@ import shutil
 import gzip
 
 
-# Open logfile and set starting time
-sys.stderr = open('make_mpcorb_extended.log', 'a')
-localtime = time.localtime(time.time())
-sys.stderr.write(strftime('%Y/%m/%d at %H:%M:%S  --  Run started\n  ', localtime))
-
 
 # Get MPCORB.DAT over the internet or locally from /base/public/iau/:
 try:
-  urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/MPCORB/MPCORBs.DAT', 'mpcorb.dat')
-  #shutil.copyfile('iau/MPCORB/MPCORB.DAT', 'mpcorb.dat')
+#  urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT', 'mpcorb.dat')
+  shutil.copyfile('iau/MPCORB/MPCORB.DAT', 'mpcorb.dat')
 except Exception as the_error:
   localtime = time.localtime(time.time())
-  sys.stderr.write(strftime('%Y/%m/%d at %H:%M:%S\n  Problem downloading file: \n  http://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT\n  Error type: '+str(the_error)+'\n', localtime))
+  with open('make_mpcorb_extended.log', 'a') as log_file:
+    log_file.write(strftime('%Y/%m/%d at %H:%M:%S\n  Problem downloading file: \n  http://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT\n  Error type: '+str(the_error)+'\n', localtime))
   sys.exit(1)
 
 # Find length of header:
@@ -40,8 +36,8 @@ with open('mpcorb.dat', 'r') as data_file:
       break
 
 try:
-  urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/ECS/MPCAT/ids.txt', 'ids.txt')
-  #shutil.copyfile('iau/ECS/MPCAT/ids.txt', 'ids.txt')
+#  urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/ECS/MPCAT/ids.txt', 'ids.txt')
+  shutil.copyfile('iau/ECS/MPCAT/ids.txt', 'ids.txt')
 except Exception as the_error:
   localtime = time.localtime(time.time())
   with open('make_mpcorb_extended.log', 'a') as log_file:
@@ -49,8 +45,8 @@ except Exception as the_error:
   sys.exit(1)
 
 try:
-  urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/ECS/MPCAT/dbl.txt', 'dbl.txt')
-  #shutil.copyfile('iau/ECS/MPCAT/dbl.txt', 'dbl.txt')
+#  urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/ECS/MPCAT/dbl.txt', 'dbl.txt')
+  shutil.copyfile('iau/ECS/MPCAT/dbl.txt', 'dbl.txt')
 except Exception as the_error:
   localtime = time.localtime(time.time())
   with open('make_mpcorb_extended.log', 'a') as log_file:
@@ -58,8 +54,8 @@ except Exception as the_error:
   sys.exit(1)
 
 try:
-  urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/ECS/MPCAT/numids.txt', 'numids.txt')
-  #shutil.copyfile('iau/ECS/MPCAT/numids.txt', 'numids.txt')
+#  urllib.request.urlretrieve ('http://www.minorplanetcenter.net/iau/ECS/MPCAT/numids.txt', 'numids.txt')
+  shutil.copyfile('iau/ECS/MPCAT/numids.txt', 'numids.txt')
 except Exception as the_error:
   localtime = time.localtime(time.time())
   with open('make_mpcorb_extended.log', 'a') as log_file:
@@ -87,6 +83,10 @@ except IOError:
   with open('make_mpcorb_extended.log', 'a') as log_file:
     log_file.write(str(localtime[0])+'/'+str(localtime[1])+'/'+str(localtime[2])+' at '+str(localtime[3])+':'+str(localtime[4])+':'+str(localtime[5])+' -- Problem opening file: \n  '+str(os.getcwd())+'/mpcorb.dat\n')
     sys.exit(1)
+
+sys.stderr = open('make_mpcorb_extended.log', 'a')
+localtime = time.localtime(time.time())
+sys.stderr.write(strftime('%Y/%m/%d at %H:%M:%S\n  ', localtime))
 
 
 output_file = open('mpcorb_extended.dat', 'w')         
@@ -158,7 +158,6 @@ output_json = []
 with open('mpcorb_extended.dat', 'r') as data_file:
   head = list(islice(data_file, num_header_lines))
   for line in data_file:
-    line_dict = {}
     if line.strip():
       p_desig = line[:7].strip()
       if len(p_desig) == 5:
@@ -212,83 +211,86 @@ with open('mpcorb_extended.dat', 'r') as data_file:
         sys.stderr.close()
         break
         
+      neo_flag = 0
       if int(hex_flags[1],16) == (8 or 9 or 10 or 12):
-        line_dict['NEO_flag'] = 1
+        neo_flag = 1
 
+
+      km_neo_flag = 0
+      opp_flag = 0
+      critical_flag = 0
+      pha_flag = 0
       if int(hex_flags[0],16) == 1:
-        line_dict['One_km_NEO_flag'] = 1
+        km_neo_flag = 1
       elif int(hex_flags[0],16) == 2:
-        line_dict['One_opposition_object_flag'] = 1
+        opp_flag = 1
       elif int(hex_flags[0],16) == 3:
-        line_dict['One_km_NEO_flag'] = 1
-        line_dict['One_opposition_object_flag'] = 1
+        km_neo_flag = 1
+        opp_flag = 1
       elif int(hex_flags[0],16) == 4:
-        line_dict['Critical_list_numbered_object_flag'] = 1
+        critical_flag = 1
       elif int(hex_flags[0],16) == 5:
-        line_dict['One_km_NEO_flag'] = 1
-        line_dict['Critical_list_numbered_object_flag'] = 1
+        km_neo_flag = 1
+        critical_flag = 1
       elif int(hex_flags[0],16) == 6:
-        line_dict['One_opposition_object_flag'] = 1
-        line_dict['Critical_list_numbered_object_flag'] = 1
+        opp_flag = 1
+        critical_flag = 1
       elif int(hex_flags[0],16) == 7:
-        line_dict['One_km_NEO_flag'] = 1
-        line_dict['One_opposition_object_flag'] = 1
-        line_dict['Critical_list_numbered_object_flag'] = 1
+        km_neo_flag = 1
+        opp_flag = 1
+        critical_flag = 1
       elif int(hex_flags[0],16) == 8:
-        line_dict['PHA_flag'] = 1
+        pha_flag = 1
       elif int(hex_flags[0],16) == 9:
-        line_dict['One_km_NEO_flag'] = 1
-        line_dict['PHA_flag'] = 1
+        km_neo_flag = 1
+        pha_flag = 1
       elif int(hex_flags[0],16) == 10:
-        line_dict['One_opposition_object_flag'] = 1
-        line_dict['PHA_flag'] = 1
+        opp_flag = 1
+        pha_flag = 1
       elif int(hex_flags[0],16) == 11:
-        line_dict['One_km_NEO_flag'] = 1
-        line_dict['One_opposition_object_flag'] = 1
-        line_dict['PHA_flag'] = 1
+        km_neo_flag = 1
+        opp_flag = 1
+        pha_flag = 1
       elif int(hex_flags[0],16) == 12:
-        line_dict['Critical_list_numbered_object_flag'] = 1
-        line_dict['PHA_flag'] = 1
+        critical_flag = 1
+        pha_flag = 1
       elif int(hex_flags[0],16) == 13:
-        line_dict['One_km_NEO_flag'] = 1
-        line_dict['Critical_list_numbered_object_flag'] = 1
-        line_dict['PHA_flag'] = 1
+        km_neo_flag = 1
+        critical_flag = 1
+        pha_flag = 1
       elif int(hex_flags[0],16) == 14:
-        line_dict['One_opposition_object_flag'] = 1
-        line_dict['Critical_list_numbered_object_flag'] = 1
-        line_dict['PHA_flag'] = 1
+        opp_flag = 1
+        critical_flag = 1
+        pha_flag = 1
       elif int(hex_flags[0],16) == 15:
-        line_dict['One_km_NEO_flag'] = 1
-        line_dict['One_opposition_object_flag'] = 1
-        line_dict['Critical_list_numbered_object_flag'] = 1
-        line_dict['PHA_flag'] = 1
+        km_neo_flag = 1
+        opp_flag = 1
+        critical_flag = 1
+        pha_flag = 1
       
       H = line[8:13].strip()
       if H != '':
-        line_dict['H'] = float(H)
+        H = float(H)
       G = line[14:19].strip()
       if G != '':
-        line_dict['G'] = float(G)
+        G = float(G)
       num_obs = line[117:122].strip()
       if num_obs != '':
-        line_dict['Num_obs'] = int(num_obs)
+        num_obs = int(num_obs)
       rms = line[137:141].strip()
       if rms != '':
-        line_dict['rms'] = float(rms)
-      U = line[105].strip()
-#      if U.isnumeric():
-#        U = int(U)
+        rms = float(rms)
       arc = line[127:136]
+      U = line[105].strip()
+      if U.isnumeric():
+        U = int(U)
       if 'days' in arc:
-        line_dict['Arc_length'] = int(arc[:4].strip())
+        arc_length = int(arc[:4].strip())
+        arc_years = ''
       else:
-        line_dict['Arc_years'] = arc
-        
-      line_dict2 = {'Number':number,'Name':name,'Designation':desig,'Other_desigs':other_desig,'Epoch':round(epoch,7),'M':float(line[26:35].strip()),'Peri':float(line[37:46].strip()),'Node':float(line[48:57].strip()),'i':float(line[59:68].strip()),'e':float(line[70:79].strip()),'n':float(line[80:91].strip()),'a':float(line[92:103].strip()),'U':U,'Ref':line[107:116].strip(),'Num_opps':int(line[123:126].strip()),'Perturbers':line[142:145].strip(),'Perturbers_2':line[146:149].strip(),'Computer':line[150:160].strip(),'Hex_flags':hex_flags,'Last_obs':last_obs,'Tp':float(line[203:216].strip()),'Orbital_period':round(period,7),'Perihelion_dist':round(q_small,7),'Aphelion_dist':round(Q_big,7),'Semilatus_rectum':round(semilatus,7),'Synodic_period':round(synodic,7)}  
-      
-      line_dict.update(line_dict2)
-      output_json.append(line_dict)
-
+        arc_length = ''
+        arc_years = arc
+      output_json.append({'Number':number,'Name':name,'Designation':desig,'Other_desigs':other_desig,'H':H,'G':G,'Epoch':round(epoch,7),'M':float(line[26:35].strip()),'Peri':float(line[37:46].strip()),'Node':float(line[48:57].strip()),'i':float(line[59:68].strip()),'e':float(line[70:79].strip()),'n':float(line[80:91].strip()),'a':float(line[92:103].strip()),'U':U,'Ref':line[107:116].strip(),'Num_obs':num_obs,'Num_opps':int(line[123:126].strip()),'Arc_length':arc_length,'Arc_years':arc_years,'rms':rms,'Perturbers':line[142:145].strip(),'Perturbers_2':line[146:149].strip(),'Computer':line[150:160].strip(),'Hex_flags':hex_flags,'Last_obs':last_obs,'Tp':float(line[203:216].strip()),'Orbital_period':round(period,7),'Perihelion_dist':round(q_small,7),'Aphelion_dist':round(Q_big,7),'Semilatus_rectum':round(semilatus,7),'Synodic_period':round(synodic,7),'NEO_flag':neo_flag,'One_km_NEO_flag':km_neo_flag,'One_opposition_object_flag':opp_flag,'Critical_list_numbered_object_flag':critical_flag,'PHA_flag':pha_flag})
 
 json.dump(output_json, output_json_file, indent=0)
 output_json_file.close()
