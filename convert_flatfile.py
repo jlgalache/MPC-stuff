61c7a7db11a9b34236c14214eb8723c30605b166
J.L. Galache <jlgalache@gmail.com>
2016-03-30 18:14:30 -0400

    Added reading in arguments from file

--- convert_flatfile.py
+++ convert_flatfile.py
@@ -6,52 +6,80 @@ import json
 import os
 import sys
 
-
+# Set up the arguments to be supplied to the routine:
 parser = argparse.ArgumentParser()
 #parser.add_argument('-in','--flat_filename', help='Name of the file you want to convert')
 #parser.add_argument('-out','--output_filename', help='Name of the output file')
-parser.add_argument('flat_filename', type=str, help='Name of the file you want to convert')
+parser.add_argument('-of','--options_file', action='store_true', default=False, dest='boolean_switch', help='Inputs provided through convert_flatfile_options.txt')
+parser.add_argument('-in','--flat_filename', default='', type=str, help='Name of the file you want to convert')
 parser.add_argument('-out','--output_filename', default='-1', type=str, help='Name of the output file; if missing, input filename will be used with a JSON extension')
 parser.add_argument('-hl','--header_lines', default=0, type=int, help='Number of header lines')
 parser.add_argument('-cnl','--col_name_line', default=-1, type=int, help='Line number where column names are found')
-parser.add_argument('-nr','--num_rows', default=-1, type=int, help='Number of data rows to read')
 parser.add_argument('-s','--separator', default='', help='Column separator')
 parser.add_argument('-cs','--col_start', nargs='*', default=-1, type=int, help='Column start')
 parser.add_argument('-cw','--col_width', nargs='*', default=-1, type=int, help='Column width')
 parser.add_argument('-ct','--col_type', nargs='*', default='', help='Column data type')
 parser.add_argument('-cn','--col_names', nargs='*', default='', type=str, help='Column names if you don\'t want to use the header or it\'s missing')
+parser.add_argument('-nr','--num_rows', default=-1, type=int, help='Number of data rows to read')
 args = parser.parse_args()
 
+if args.options_file:
+  with open('convert_flatfile_options.txt','r') as options_file:
+    for line in options_file:
+      if line.startswith('flat_filename'):
+        args.flat_filename = line.split(':')[1].strip().strip('\'"')
+      elif line.startswith('output_filename'):
+        args.output_filename = line.split(':')[1].strip().strip('\'"')
+      elif line.startswith('header_lines'):
+        args.header_lines = int(line.split(':')[1].strip().strip('\'"'))
+      elif line.startswith('col_name_line'):
+        args.col_name_line = int(line.split(':')[1].strip().strip('\'"'))
+      elif line.startswith('separator'):
+        args.separator = line.split(':')[1].strip()strip('\'"')
+      elif line.startswith('col_start'):
+        args.col_start = list(map(int, line.split(':')[1].strip().split(' ')))
+      elif line.startswith('col_width'):
+        args.col_width = list(map(int, line.split(':')[1].strip().split(' ')))
+      elif line.startswith('col_type'):
+        args.col_type = [i.strip('\'"') for i in line.split(':')[1].strip().split(' ')] # Get rid of spurious ' or "
+      elif line.startswith('col_names'):
+        args.col_names = [i.strip('\'"') for i in line.split(':')[1].strip().split(' ')] # Get rid of spurious ' or "
+      elif line.startswith('num_rows'):
+        args.num_rows = int(line.split(':')[1].strip().strip('\'"'))
+
+# Catch errors that will be caused by missing arguments:
+if args.flat_filename == '':
+  print(' ERROR: No input file is supplied; a file name must be supplied via the -in option.')
+
 if args.header_lines == 0 and args.col_names == '':
-  print(' ERROR: If the file has no header, column names must be supplied via -cn option.')
+  print(' ERROR: If the file has no header, column names must be supplied via the -cn option.')
   sys.exit(1)
   
 if args.separator == '' and (args.col_start == -1 or args.col_width == -1):
-  print(' ERROR: If no separator is defined, then the column start points and widths must be supplied via the -cs and -cw options.')
+  print(' ERROR: If no separator is supplied, then the column start points and widths must be supplied via the -cs and -cw options.')
   sys.exit(1)
 
 if args.separator != '' and args.col_type == '':
   print(' ERROR: If you define a separator, you must also supply column data types via the -cn option.')
   sys.exit(1)
 
+if args.col_name_line != -1 and args.col_names == '':
+  print(' ERROR: No column names are supplied; column names must be supplied via the -cn option or read from the file header via the -cnl option.')
+  sys.exit(1)
+
 if args.col_type == '':
-  print(' WARNING: No column data type was specified. All columns will be imported as strings.')
+  print(' WARNING: No column data type was supplied; all columns will be imported as strings.')
   args.col_type = ['str' for i in range(0, len(args.col_names))]
 
-print(' Input file: '+args.flat_filename)
+
+
+# Make filename for the JSON file if user doesn't specify a name:
 if args.output_filename == '-1':
   args.output_filename = os.path.splitext(args.flat_filename)[0]+'.json'
-print(' Output file: '+args.output_filename)
-print(' Column starts: '+str(args.col_start)+' and length is: '+str(len(args.col_start)))
-print(' Column width: '+str(args.col_width)+' and length is: '+str(len(args.col_width)))
-print(' Column type: '+str(args.col_type)+' and length is: '+str(len(args.col_type)))
-print(' Column names: '+str(args.col_names)+' and length is: '+str(len(args.col_names)))
-print(' CNL: '+str(args.col_name_line))
-
 
 input_file = open(args.flat_filename, 'r')
 
-# Get column names from the appropriate line or from the provided names
+# Get column names from the appropriate line in the file or from the names provided via argument:
 if args.col_name_line != -1:
   for read_count, line in enumerate(input_file):
     if (read_count+1) == args.col_name_line:
@@ -60,13 +88,23 @@ if args.col_name_line != -1:
         headings.append(str.strip(line[j:j+args.col_width[i]]))
       break
 else:
-  if args.col_names == '':
-    print('\n You\'re missing -cnl *and* -cn. You need one or the other to define the column names.\n')
-    sys.exit(1)
   headings = args.col_names
   read_count = 0
 
-# Get to the line where the data starts
+# Debugging prinouts:
+print(' Input file: '+args.flat_filename)
+print(' New Input file: '+args.flat_filename)
+print(' Output file: '+args.output_filename)
+print(' Column starts: '+str(args.col_start)+' and length is: '+str(len(args.col_start)))
+print(' Column width: '+str(args.col_width)+' and length is: '+str(len(args.col_width)))
+print(' Column type: '+str(args.col_type)+' and length is: '+str(len(args.col_type)))
+print(' Column names: '+str(args.col_names)+' and length is: '+str(len(args.col_names)))
+print(' CNL: '+str(args.col_name_line))
+
+
+
+
+# Get to the line where the data starts:
 if args.header_lines > read_count:
   for i, line in enumerate(input_file): 
     if i == (args.header_lines - read_count - 2):
@@ -75,8 +113,10 @@ if args.header_lines > read_count:
 output_json_file = open(args.output_filename, 'w')
 output_json = []
 
-# Start reading in data
-for line in input_file:
+# Start reading in data:
+for count, line in enumerate(input_file):
+  if count == args.num_rows:
+    break # Read only specified number of rows if -rn option is set
   line_dict = {}
   for  i, j in enumerate(args.col_start):
     if str.strip(line[j:j+args.col_width[i]]) != '':
